70208fb59fa0dcc3bfbc59ebf14a51984215b227
joelikenga <joelikenga@gmail.com>
2024-05-04 18:23:12 +0300

    fix:(animate forgot pass)

--- src/assets/components/login/index.tsx
+++ src/assets/components/login/index.tsx
@@ -162,7 +162,7 @@ export const Login: React.FC<Props> = ({ loginToogleFunction }) => {
         onClick={() => setShowModal(false)}
         className={
           showModal
-            ? "fixed h-screen w-screen flex items-center justify-center cursor-not-allowed bg-[rgba(0,0,0,0.6)] top-0 left-0"
+            ? "reveal fixed h-screen w-screen flex items-center justify-center cursor-not-allowed bg-[rgba(0,0,0,0.6)] top-0 left-0"
             : "hidden"
         }
       >
